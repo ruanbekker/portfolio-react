e9b294cf76aca24ee1d20c6242dd8a7b4ba7b4b1
Ruan Bekker <ruanbekker@users.noreply.github.com>
2025-03-07 21:59:53 +0000

    :sparkles: add contact page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import DarkModeToggle from "./DarkModeToggle";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
   return (
     <nav className="w-full flex justify-between items-center p-4 bg-white dark:bg-gray-900 shadow-md fixed top-0">
       {/* Left: Name */}
-      <div className="text-xl font-bold">
-        <Link to="/" className="text-turquoise">Ruan Bekker</Link>
+      <div className="text-2xl font-bold">
+        <Link to="/" className="text-turquoise hover:scale-105 transition-transform duration-300 ease-in-out drop-shadow-lg">
+          Ruan Bekker
+        </Link>
       </div>
 
       {/* Right-Aligned Menu Items */}
       <div className="flex space-x-6 ml-auto mr-16">
-        <Link to="/" className="text-gray-700 dark:text-gray-300 hover:text-turquoise">Home</Link>
-        <a href="#projects" className="text-gray-700 dark:text-gray-300 hover:text-turquoise">Projects</a>
-        <Link to="/resume" className="text-gray-700 dark:text-gray-300 hover:text-turquoise">Resume</Link>
-        <a href="#contact" className="text-gray-700 dark:text-gray-300 hover:text-turquoise">Contact</a>
+        <Link to="/" className="text-gray-700 dark:text-gray-300 hover:text-turquoise transition duration-300 ease-in-out transform hover:scale-105">Home</Link>
+        <Link to="/projects" className="text-gray-700 dark:text-gray-300 hover:text-turquoise transition duration-300 ease-in-out transform hover:scale-105">Projects</Link>
+	<Link to="/resume" className="text-gray-700 dark:text-gray-300 hover:text-turquoise transition duration-300 ease-in-out transform hover:scale-105">Resume</Link>
+        <Link to="/contact" className="text-gray-700 dark:text-gray-300 hover:text-turquoise transition duration-300 ease-in-out transform hover:scale-105">Contact</Link>
       </div>
 
       {/* Far Right: Dark Mode Toggle */}
