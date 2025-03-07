47d80b33792189e1b2797d0dab8c833a6cb48d28
Ruan Bekker <ruanbekker@users.noreply.github.com>
2025-03-07 21:39:54 +0000

    :wrench: add configs

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        turquoise: "#00b4d8",
+      },
+    },
+  },
+  plugins: [],
+};
+
+
